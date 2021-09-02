2bbb04a35ac58ff4c3600714e2138fdd0863145e
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2021-09-02 07:57:08 -0500

    Initial commit

---
+++ proyectoBase3CM13/src/main/java/com/ipn/mx/modelo/dto/CategoriaDTO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ipn.mx.modelo.dto;
+
+import com.ipn.mx.modelo.entidades.Categoria;
+import java.io.Serializable;
+
+/**
+ *
+ * @author caleb
+ */
+public class CategoriaDTO implements Serializable{//transfer object
+    private Categoria entidadad;
+
+    public CategoriaDTO() {
+        entidadad = new Categoria();
+    }
+
+    public Categoria getEntidadad() {
+        return entidadad;
+    }
+
+    public void setEntidadad(Categoria entidadad) {
+        this.entidadad = entidadad;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("clave: ").append(getEntidadad().getIdCategoria()).append("\n");
+        sb.append("clave: ").append(getEntidadad().getNombreCategoria()).append("\n");
+        sb.append("clave: ").append(getEntidadad().getDescripcionCategoria()).append("\n");
+        return sb.toString();
+    }
+    
+    
+}
